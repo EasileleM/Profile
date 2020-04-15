9796494557bc5e2715614ddd2d3d53724455b8fa
Savva Dzhumaliev <pro100prosavva@gmail.com>
2020-04-15 22:25:46 +0400

    remove console log

--- User/user.js
+++ User/user.js
@@ -1,13 +1,16 @@
 const login = sessionStorage.getItem('isLogined');
 if (login) {
     const users = JSON.parse(localStorage.getItem('users'));
-    console.log(users);
     const user = users.find((item) => login === item.login);
     document.querySelector('#firstName').innerHTML = user.firstName;
     document.querySelector('#lastName').innerHTML = user.lastName;
 } else {
     window.location.replace(`../Login/login.html`);
 }
+
+document.addEventListener('onmousemove', (e) => {
+    document.querySelector('#mouseCoordinates').innerHTML = e.clientX + ' ' + e.clientY;
+});
 
 function logout() {
     sessionStorage.clear();
